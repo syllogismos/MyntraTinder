7626d5fb8747c72679d6174c1b9743ef6eeaba9d
Anil Karaka <anilkaraka@live.in>
2014-07-19 04:20:27 +0530

    parse json file into a List of products

--- app/src/main/java/anil/myntratinder/ProductsJSONPullParser.java
+++ app/src/main/java/anil/myntratinder/ProductsJSONPullParser.java
@@ -2,6 +2,16 @@ package anil.myntratinder;
 
 import android.content.Context;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -38,7 +48,41 @@ public class ProductsJSONPullParser {
         /* TODO: parse json file and extract products,
          * http://www.androidhive.info/2012/01/android-json-parsing-tutorial/
          */
+        List<Product> products = new ArrayList<Product>();
+        String json = null;
+        Product product = null;
+        try {
+            FileInputStream fis = context.openFileInput("products.json");
+            int size = fis.available();
+            byte[] buffer = new byte[size];
+            fis.read(buffer);
+            fis.close();
+            json = new String(buffer, "UTF-8");
+            if (json != null){
+                JSONObject jsonObject = new JSONObject(json);
+                JSONObject response1 = jsonObject.getJSONObject("response1");
+                JSONArray productObjects = response1.getJSONArray("products");
+                for (int i = 0; i < productObjects.length(); i++) {
+                    JSONObject p = productObjects.getJSONObject(i);
+                    product = new Product(i);
+                    product.setImageUrl(p.getString("search_image"));
+                    product.setPrice(p.getString("discounted_price"));
+                    product.setName(p.getString("product"));
+                    products.add(product);
+                    /*
+                     * todo: get image url from the json object imageEntry_default, rather than search_image
+                     * todo: add more getter and setters to the Product class to extract even more data from json object
+                     */
+                }
 
-        return null;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return products;
     }
 }
