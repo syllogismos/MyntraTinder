ed8f0f1adc990a997080feba85871411a83eb9d8
Anil Karaka <anilkaraka@live.in>
2014-07-19 05:35:00 +0530

    single product card view

--- app/src/main/java/anil/myntratinder/SingleProductView.java
+++ app/src/main/java/anil/myntratinder/SingleProductView.java
@@ -1,7 +1,64 @@
 package anil.myntratinder;
 
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.androidannotations.annotations.EViewGroup;
+import org.androidannotations.annotations.ViewById;
+
 /**
  * Created by Anil on 7/18/2014.
  */
-public class SingleProductView {
+@EViewGroup(R.layout.product_card)
+public class SingleProductView extends RelativeLayout implements ProductStackView.ProductStackListener{
+
+    @ViewById
+    ImageView picture;
+
+    @ViewById
+    TextView yes;
+
+    @ViewById
+    TextView no;
+
+    private Product product;
+
+    public SingleProductView(Context context) {
+        super(context);
+    }
+
+    public void bind(Product mProduct){
+        product = mProduct;
+        return;
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        // todo: you can download the picture here or in the getView function of the ProductCardAdapter, figure out whats best.
+    }
+
+    @Override
+    public void onUpdateProgress(boolean positif, float percent, View view) {
+        if (positif) {
+            yes.setAlpha(percent);
+        } else {
+            no.setAlpha(percent);
+        }
+    }
+
+    @Override
+    public void onCancelled(View beingDragged) {
+        yes.setAlpha(0);
+        no.setAlpha(0);
+    }
+
+    @Override
+    public void onChoiceMade(boolean choice, View beingDragged) {
+        yes.setAlpha(0);
+        no.setAlpha(0);
+    }
 }
