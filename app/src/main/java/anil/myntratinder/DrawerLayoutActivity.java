a2b679dae87c1b5407f00bf70c6093ee5420f53b
Anil Karaka <anilkaraka@live.in>
2014-07-24 08:22:32 +0530

    drawer layout with the list items in drawer implemented

--- app/src/main/java/anil/myntratinder/DrawerLayoutActivity.java
+++ app/src/main/java/anil/myntratinder/DrawerLayoutActivity.java
@@ -1,19 +1,146 @@
 package anil.myntratinder;
 
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 import anil.myntratinder.R;
+import anil.myntratinder.adapter.NavDrawerListAdapter;
+import anil.myntratinder.model.NavDrawerItem;
 
 public class DrawerLayoutActivity extends Activity {
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] navMenuTitles;
+    private TypedArray navMenuIcons;
+
+    private ArrayList<NavDrawerItem> navDrawerItems;
+    private NavDrawerListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer_layout);
+
+        mTitle = mDrawerTitle = getTitle();
+
+        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
+
+        navMenuIcons = getResources().obtainTypedArray(R.array.nav_drawer_icons);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.list_slidermenu);
+
+        navDrawerItems = new ArrayList<NavDrawerItem>();
+        // Home
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0,-1)));
+        // Male
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1,-1)));
+        // Female
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2,-1)));
+        // Kids with counter for shits and giggles
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3,-1), "22", true));
+        // Settings
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[4], navMenuIcons.getResourceId(4, -1)));
+
+        navMenuIcons.recycle();
+
+        adapter = new NavDrawerListAdapter(getApplicationContext(),
+                navDrawerItems);
+
+        mDrawerList.setAdapter(adapter);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_drawer,
+                R.string.app_name,
+                R.string.app_name){
+
+            /**
+             * {@link android.support.v4.widget.DrawerLayout.DrawerListener} callback method. If you do not use your
+             * ActionBarDrawerToggle instance directly as your DrawerLayout's listener, you should call
+             * through to this method from your own listener object.
+             *
+             * @param drawerView Drawer view that is now open
+             */
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu();
+            }
+
+            /**
+             * {@link android.support.v4.widget.DrawerLayout.DrawerListener} callback method. If you do not use your
+             * ActionBarDrawerToggle instance directly as your DrawerLayout's listener, you should call
+             * through to this method from your own listener object.
+             *
+             * @param drawerView Drawer view that is now closed
+             */
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        if (savedInstanceState == null){
+            displayView(0);
+        }
+
     }
 
+    private class SlideMenuClickListener implements ListView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            displayView(i);
+        }
+    }
+
+    private void displayView(int position) {
+
+    }
+
+    // todo: Override methods onPrepareOptionsMenu, setTitle, onPostCreate, onConfigurationChanged
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        super.setTitle(title);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
