6a9f50a4c0bd9d7226483a0bed840870c90b56be
Anil Karaka <anilkaraka@live.in>
2014-07-24 08:29:56 +0530

    implement home fragment

---
+++ app/src/main/java/anil/myntratinder/HomeFragment.java
@@ -0,0 +1,33 @@
+package anil.myntratinder;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+
+}
