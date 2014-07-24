b408da5992ce59e25f8f7e2317c58a48274fdb57
Anil Karaka <anilkaraka@live.in>
2014-07-24 07:40:54 +0530

    adapter class for drawer nav list

---
+++ app/src/main/java/anil/myntratinder/adapter/NavDrawerListAdapter.java
@@ -0,0 +1,66 @@
+package anil.myntratinder.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import anil.myntratinder.R;
+import anil.myntratinder.model.NavDrawerItem;
+
+/**
+ * Created by Anil on 7/24/2014.
+ */
+public class NavDrawerListAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<NavDrawerItem> navDrawerItems;
+
+    public NavDrawerListAdapter(Context context, ArrayList<NavDrawerItem> navDrawerItems) {
+        this.context = context;
+        this.navDrawerItems = navDrawerItems;
+    }
+
+    @Override
+    public int getCount() {
+        return navDrawerItems.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return navDrawerItems.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater mInflater = (LayoutInflater)
+                    context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = mInflater.inflate(R.layout.drawer_list_item, null);
+        }
+
+        ImageView imgIcon = (ImageView) convertView.findViewById(R.id.list_item_icon);
+        TextView txtTitle = (TextView) convertView.findViewById(R.id.list_item_title);
+        TextView txtCount = (TextView) convertView.findViewById(R.id.list_item_counter);
+
+        imgIcon.setImageResource(navDrawerItems.get(position).getIcon());
+        txtTitle.setText(navDrawerItems.get(position).getTitle());
+
+        if (navDrawerItems.get(position).isCounterVisible()){
+            txtCount.setText(navDrawerItems.get(position).getCount());
+        } else {
+            txtCount.setVisibility(View.GONE);
+        }
+        return convertView;
+    }
+}
