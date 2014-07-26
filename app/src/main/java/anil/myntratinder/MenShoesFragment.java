a1514ae3a9b4f2a0b5442cd9d97b0047761b9acb
Anil Karaka <anilkaraka@live.in>
2014-07-26 07:37:23 +0530

    add small explanation

--- app/src/main/java/anil/myntratinder/MenShoesFragment.java
+++ app/src/main/java/anil/myntratinder/MenShoesFragment.java
@@ -50,6 +50,7 @@ public class MenShoesFragment extends Fragment {
     private void doInitialize() {
         //todo: getInstance(context) did some type inference and came up with below statement, don't know if it works
         // todo: add url, postdata, filename parameters after figuring out how to generalize it.
+        // todo: change this.getActivity().getBaseContext() to getActivity() if things don't work
         menShoesStackView.setAdapter(ProductCardAdapter_.getInstance_(this.getActivity().getBaseContext()));
     }
 }
