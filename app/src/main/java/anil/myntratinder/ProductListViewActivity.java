864e686e0ae3ad667868a0bf6901f821b4163437
Anil Karaka <anilkaraka@live.in>
2014-07-28 14:24:42 +0530

    hard coded url, postData and fileName in listview activity

--- app/src/main/java/anil/myntratinder/ProductListViewActivity.java
+++ app/src/main/java/anil/myntratinder/ProductListViewActivity.java
@@ -21,6 +21,10 @@ public class ProductListViewActivity extends Activity {
     private ProductListAdapter productListAdapter;
     private ListView productListView;
 
+    public String url = "http://www.myntra.com/searchws/search/styleids2";
+    public String postData = "[{\"query\":\"(global_attr_age_group:(\\\"Adults-Men\\\" OR \\\"Adults-Unisex\\\") AND global_attr_article_type_facet:(\\\"Casual Shoes\\\") AND global_attr_master_category:(\\\"Footwear\\\" OR \\\"Free Items\\\"))\",\"start\":0,\"rows\":96,\"facet\":true,\"facetField\":[\"Casual_Shoe_Type_article_attr\",\"Upper_Material_article_attr\",\"Fastening_article_attr\",\"Ankle_Height_article_attr\",\"Width_article_attr\"],\"fq\":[\"discounted_price:[499 TO 8199]\",\"count_options_availbale:[1 TO *]\"],\"sort\":[{\"sort_field\":\"count_options_availbale\",\"order_by\":\"desc\"},{\"sort_field\":\"style_store3_male_sort_field\",\"order_by\":\"desc\"},{\"sort_field\":\"potential_revenue_male_sort_field\",\"order_by\":\"desc\"},{\"sort_field\":\"global_attr_catalog_add_date\",\"order_by\":\"desc\"}],\"return_docs\":true,\"colour_grouping\":true}]";
+    public String fileName = "products.json";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,7 +57,7 @@ public class ProductListViewActivity extends Activity {
         @Override
         protected Void doInBackground(Void... voids) {
             try {
-                Downloader.downloadFromUrl("http://", "postdata", openFileOutput("products.json", Context.MODE_PRIVATE));
+                Downloader.downloadFromUrl(url, postData, openFileOutput(fileName, Context.MODE_PRIVATE));
                 // todo: url, postdata and filename
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
