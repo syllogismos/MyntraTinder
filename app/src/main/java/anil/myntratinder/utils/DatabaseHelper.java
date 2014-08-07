2d0bbaabd8edb36241e755a14c138264ff25f451
Anil Karaka <anilkaraka@live.in>
2014-08-07 23:53:32 +0530

    add todos and database version number

--- app/src/main/java/anil/myntratinder/utils/DatabaseHelper.java
+++ app/src/main/java/anil/myntratinder/utils/DatabaseHelper.java
@@ -9,17 +9,20 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    public static final int DATABASE_VERSION = 1;
+
     public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        // todo: create tables when database is first created
+        // things to do generally when a database is created..
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
-
+        // todo: handle what to do when you upgrade the database
     }
 }
