1b0ef898723ea19ce60afc65db97418c18df568c
Anil Karaka <anilkaraka@live.in>
2014-08-29 23:58:58 +0530

    a list Adapter that implements cursor adapter

---
+++ app/src/main/java/anil/myntratinder/adapters/ProductListAdapterWithACursor.java
@@ -0,0 +1,27 @@
+package anil.myntratinder.adapters;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+/**
+ * Created by Anil on 8/29/2014.
+ */
+public class ProductListAdapterWithACursor extends CursorAdapter {
+
+    public ProductListAdapterWithACursor(Context context, Cursor c, boolean autoRequery) {
+        super(context, c, autoRequery);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
