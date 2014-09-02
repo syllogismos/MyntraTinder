9ff8bfe18f327bd11e01b8cf2219685aefad3975
Anil Karaka <anilkaraka@live.in>
2014-09-02 14:51:13 +0530

    add simple todo/fixme

--- app/src/main/java/anil/myntratinder/NavigationDrawerFragmentSingleElv.java
+++ app/src/main/java/anil/myntratinder/NavigationDrawerFragmentSingleElv.java
@@ -324,6 +324,7 @@ public class NavigationDrawerFragmentSingleElv extends Fragment {
         if (item.getItemId() == R.id.action_example) {
             // Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
             Activity myntraActivity = getActivity();
+            //fixme: get productGroup from container not from curGroup, curChild
             View containerView = myntraActivity.findViewById(R.id.container);
             View navigationDrawerFragment = myntraActivity.findViewById(R.id.navigation_drawer); // todo: get the productCardView directly from here instead of this way of doing it
             ExpandableListView elv = (ExpandableListView) navigationDrawerFragment.findViewById(R.id.expandableListView);
